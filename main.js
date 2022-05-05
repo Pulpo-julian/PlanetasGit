bdce1743863c82face86078534569d97fc062f02
unknown <SENA@RIOAPRREAFSD030.FormacionSena.RED>
2022-05-05 12:04:35 -0500

    primer commit

---
+++ main.js
@@ -0,0 +1,42 @@
+var mercurio = document.getElementById("mercurio");
+var venus = document.getElementById("venus");
+var mercurio = document.getElementById("mercurio");
+var mercurio = document.getElementById("mercurio");
+var mercurio = document.getElementById("mercurio");
+var mercurio = document.getElementById("mercurio");
+var mercurio = document.getElementById("mercurio");
+var mercurio = document.getElementById("mercurio");
+var mercurio = document.getElementById("mercurio");
+
+
+var salida = document.getElementById("salida");
+var entrada = document.getElementById("entrada");
+var pesoEntrada;
+var pesoFinal;
+
+mercurio.onclick = () => {
+
+    console.log(entrada.value)
+
+    pesoEntrada = parseFloat(entrada.value);
+
+    pesoFinal = (3.7 * pesoEntrada) / 9.8;
+
+    salida.innerHTML = `tu peso en Mercurio es ${pesoFinal} Kilos`;
+
+}
+
+
+mercurio.onclick = () => {
+
+    console.log(entrada.value)
+
+    pesoEntrada = parseFloat(entrada.value);
+
+    pesoFinal = (3.7 * pesoEntrada) / 9.8;
+
+    salida.innerHTML = `tu peso en Mercurio es ${pesoFinal} Kilos`;
+
+}
+
+
